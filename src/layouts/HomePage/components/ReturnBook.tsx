6bbe65f89d58d988849d2843f1efd6474e33b93e
MBELABED <Mustapha.13000@gmail.com>
2023-11-08 12:25:44 +0100

    Getting carousel books from rest api

--- src/layouts/HomePage/components/ReturnBook.tsx
+++ src/layouts/HomePage/components/ReturnBook.tsx
@@ -1,11 +1,16 @@
 import React from "react"
-export const ReturnBook = () => {
+import BookModel from "../../../Models/BookModel";
+export const ReturnBook  :React.FC<{book:BookModel}> = (props) => {
     return (
         <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
             <div className="text-center">
-                <img src={require('./../../../Images/BooksImages/book-luv2code-1000.png')} width='151' height='133' alt="book"></img>
-                <h6 className="mt-2">Book</h6>
-                <p>Lov 2 Code</p>
+                {props.book.img?
+                    <img src={props.book.img} width='151' height='133' alt="book"></img>
+                    :
+                    <img src={require('./../../../Images/BooksImages/book-luv2code-1000.png')} width='151' height='133' alt="book"></img>
+                }
+                <h6 className="mt-2">{props.book.title}</h6>
+                <p>{props.book.author}</p>
                 <a className="btn main-color text-white" href="#">Reserve</a>
             </div>
         </div>
